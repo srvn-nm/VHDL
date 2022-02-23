fe672b09873d63b6c0aa4aac1986e8458b4ca9fd
Sarvin Nami <srvn0nm@gmail.com>
2022-02-23 23:07:57 +0330

    editting 2 input or gate

---
+++ FullAdder_Lab1/Xor.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:06:31 02/23/2022 
+// Design Name: 
+// Module Name:    Xor 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Xor(
+    input a,
+    input b,
+    output c
+    );
+
+
+endmodule
