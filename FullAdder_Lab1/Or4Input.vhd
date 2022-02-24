7e7feb3e38fe9015265c19fc75cb5183330e9e6b
Sarvin Nami <srvn0nm@gmail.com>
2022-02-24 14:34:21 +0330

    adding 4 input or gate

---
+++ FullAdder_Lab1/Or4Input.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:34:02 02/24/2022 
+-- Design Name: 
+-- Module Name:    Or4Input - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Or4Input is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           c : in  STD_LOGIC;
+           d : in  STD_LOGIC;
+           output : out  STD_LOGIC);
+end Or4Input;
+
+architecture Behavioral of Or4Input is
+
+begin
+
+
+end Behavioral;
+
