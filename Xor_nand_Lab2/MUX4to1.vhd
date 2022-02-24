d8f90e95b973b5366dd52ee7ff28f960b7b44e5c
Sarvin Nami <srvn0nm@gmail.com>
2022-02-24 16:43:58 +0330

    coding the MUX

--- Xor_nand_Lab2/MUX4to1.vhd
+++ Xor_nand_Lab2/MUX4to1.vhd
@@ -42,7 +42,19 @@ end MUX4to1;
 architecture Behavioral of MUX4to1 is
 
 begin
-
+process (A,B,C,D,S0,S1) is
+begin
+  if (S0 ='0' and S1 = '0') then
+      Z <= A;
+  elsif (S0 ='1' and S1 = '0') then
+      Z <= B;
+  elsif (S0 ='0' and S1 = '1') then
+      Z <= C;
+  else
+      Z <= D;
+  end if;
+ 
+end process;
 
 end Behavioral;
 
