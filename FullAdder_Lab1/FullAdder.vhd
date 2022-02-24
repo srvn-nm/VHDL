8514751a2c63b317c673b68f3a056774e44aa793
Sarvin Nami <srvn0nm@gmail.com>
2022-02-24 14:08:10 +0330

    bugs fixed

--- FullAdder_Lab1/FullAdder.vhd
+++ FullAdder_Lab1/FullAdder.vhd
@@ -54,8 +54,8 @@ SIGNAL S0,S1,S2:STD_LOGIC;
 begin
 
 U1:HalfAdder PORT MAP(A=>A,B=>B,S=>S0,C=>1);
-U2:HalfAdder PORT MAP(A=>S0,B=>C,S=>S,C=>S2);
-U3:Or2Input PORT MAP(A=>S2,B=>S1,C=>C);
+U2:HalfAdder PORT MAP(A=>S0,B=>C,S=>sum,C=>S2);
+U3:Or2Input PORT MAP(A=>S2,B=>S1,C=>carry);
 
 end structural;
 
