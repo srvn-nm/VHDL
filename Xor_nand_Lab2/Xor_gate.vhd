44fc09f596ccceaf81cb8d23ef5add4cc657ce5b
Sarvin Nami <srvn0nm@gmail.com>
2022-02-24 15:54:32 +0330

    creating xor gate using nand gates

---
+++ Xor_nand_Lab2/Xor_gate.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:54:14 02/24/2022 
+-- Design Name: 
+-- Module Name:    Xor_gate - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Xor_gate is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           c : out  STD_LOGIC);
+end Xor_gate;
+
+architecture Behavioral of Xor_gate is
+
+begin
+
+
+end Behavioral;
+
