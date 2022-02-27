bff2c5f2700dc46a7bff8099f9bd0f058a52dea0
Sarvin Nami <srvn0nm@gmail.com>
2022-02-28 00:48:08 +0330

    recieving errors

--- Xor_nand_Lab2/fuseRelaunch.cmd
+++ Xor_nand_Lab2/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/ASUS.PIESC/OneDrive/Desktop/Programing/VHDL/Xor_nand_Lab2/decoder_test_bench_isim_beh.exe" -prj "C:/Users/ASUS.PIESC/OneDrive/Desktop/Programing/VHDL/Xor_nand_Lab2/decoder_test_bench_beh.prj" "work.decoder_test_bench" 
+-intstyle "ise" -incremental -o "C:/Users/ASUS.PIESC/OneDrive/Desktop/Programing/VHDL/Xor_nand_Lab2/fullAdder_testbench_isim_beh.exe" -prj "C:/Users/ASUS.PIESC/OneDrive/Desktop/Programing/VHDL/Xor_nand_Lab2/fullAdder_testbench_beh.prj" "work.fullAdder_testbench" 
