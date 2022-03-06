7ec6a3f870aaa8a41ad955eebdb3967745952e81
Sarvin Nami <srvn0nm@gmail.com>
2022-03-06 20:26:52 +0330

    new project for creating a 4 bit counter using t flip flop

---
+++ TFLipFlopCounter_Lab3/T_FF.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:21:05 03/06/2022 
+-- Design Name: 
+-- Module Name:    T_FF - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity T_FF is
+end T_FF;
+
+architecture Behavioral of T_FF is
+
+begin
+
+
+end Behavioral;
+
