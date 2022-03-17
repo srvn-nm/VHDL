e5bdee05f0bfb7af110664e2c2a70ddd419978e8
Sarvin Nami <srvn0nm@gmail.com>
2022-03-18 01:06:29 +0330

    test  bench for new mealy machine

--- sequenceDetector_Lab4/Q3_mealyMachine.vhd
+++ sequenceDetector_Lab4/Q3_mealyMachine.vhd
@@ -29,13 +29,47 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Q3_mealyMachine is
-end Q3_mealyMachine;
+entity Q3_mealyMachine is port (
+     clk,x:    in std_logic;
+     z:        out std_logic);
+end Q3_mealyMachine;              
 
 architecture Behavioral of Q3_mealyMachine is
-
+     type states is (a,b,c);
+     signal state:  states := a;  -- initial value is a
+     signal next_state:   states := a;  -- initial value
 begin
-
-
+clock:  process(clk)
+          begin
+            if clk'event and clk = '1' then
+            state <= next_state;
+            end if;
+          end process clock;
+state_trans:  process(state,x)   --reacts to changes in state and x
+          begin
+            next_state <= state;  --update next state
+            case state is
+               when a => if x = '1' then
+                         z <= '0';
+                         next_state <= b;
+                    else
+                         next_state <= a;
+                         z <= '0';
+                    end if;
+               when b => if x = '1' then
+                         next_state <= b;
+                         z <= '1';
+                    else
+                         z <= '1';
+                         next_state <= a;
+                    end if;
+               when c => if x = '1' then
+                         next_state <= b;
+                         z <= '0';
+                    else
+                         next_state <= a;
+                         z <= '0';
+                    end if;
+end case;
+end process state_trans;
 end Behavioral;
-
