32131897fa745b4403041fa31f6ceddeb26edaf7
Sarvin Nami <srvn0nm@gmail.com>
2022-03-18 00:21:46 +0330

    mealy fsm sequence detector

---
+++ sequenceDetector_Lab4/VHDL_MEALY_FSM_Sequence_Detector.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:21:25 03/18/2022 
+-- Design Name: 
+-- Module Name:    VHDL_MEALY_FSM_Sequence_Detector - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity VHDL_MEALY_FSM_Sequence_Detector is
+end VHDL_MEALY_FSM_Sequence_Detector;
+
+architecture Behavioral of VHDL_MEALY_FSM_Sequence_Detector is
+
+begin
+
+
+end Behavioral;
+
