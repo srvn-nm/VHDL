a1359c40517e6f47566642b19dacb73fb5bb02fc
Sarvin Nami <srvn0nm@gmail.com>
2022-04-17 12:05:15 +0430

    starting bcd adder

---
+++ BCDAdder_Lab5/BCDAdder.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:02:36 04/17/2022 
+-- Design Name: 
+-- Module Name:    BCDAdder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity BCDAdder is
+    Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);
+           b : in  STD_LOGIC_VECTOR (3 downto 0);
+           sum : out  STD_LOGIC_VECTOR (3 downto 0);
+           carry : in  STD_LOGIC);
+end BCDAdder;
+
+architecture Behavioral of BCDAdder is
+
+begin
+
+
+end Behavioral;
+
