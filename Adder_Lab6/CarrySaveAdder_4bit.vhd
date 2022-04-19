b710ee9a99bcacc8ca21cece5fa6df5cfeceb592
Sarvin Nami <srvn0nm@gmail.com>
2022-04-19 13:46:35 +0430

    starting the sarry save adder

---
+++ Adder_Lab6/CarrySaveAdder_4bit.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:45:32 04/19/2022 
+-- Design Name: 
+-- Module Name:    CarrySaveAdder_4bit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity CarrySaveAdder_4bit is
+    Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
+           B : in  STD_LOGIC_VECTOR (3 downto 0);
+           C : in  STD_LOGIC_VECTOR (3 downto 0);
+           S : out  STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out  STD_LOGIC_VECTOR (3 downto 0));
+end CarrySaveAdder_4bit;
+
+architecture Behavioral of CarrySaveAdder_4bit is
+
+begin
+
+
+end Behavioral;
+
