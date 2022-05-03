22d6aa9dd301fa8b45eeab51d706da3fede4f038
Sarvin Nami <srvn0nm@gmail.com>
2022-05-03 12:37:32 +0430

    shift register fisrt code

---
+++ ShiftRegister_Lab8/shiftRegister.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:36:19 05/03/2022 
+// Design Name: 
+// Module Name:    shiftRegister 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module shiftRegister(
+    );
+
+
+endmodule
