e2b4a8e8640670e3665130f2f696a55022b52a5b
Sarvin Nami <srvn0nm@gmail.com>
2022-05-03 12:34:33 +0430

    new project shift register d flip flop vhdl code

---
+++ HW8/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/ABTIN/Documents/amirkabir un/term 4/Computer Architecture/Lab/HW8/HW8/TestBench_isim_beh.exe" -prj "C:/Users/ABTIN/Documents/amirkabir un/term 4/Computer Architecture/Lab/HW8/HW8/TestBench_beh.prj" "work.TestBench" 
