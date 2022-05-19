3aab9178fc8d42a19f1a2a0afc96361289a648d7
Sarvin Nami <srvn0nm@gmail.com>
2022-05-19 23:10:30 +0430

    adding half adder and first changes to alu

--- BitALU_Lab10/ALU_8bit.vhd
+++ BitALU_Lab10/ALU_8bit.vhd
@@ -32,12 +32,45 @@ use ieee.NUMERIC_STD.all;
 --use UNISIM.VComponents.all;
 
 entity ALU_8bit is
+generic ( 
+     constant N: natural := 1  -- number of shited or rotated bits
+    );
+  
+    Port (
+    A, B     : in  STD_LOGIC_VECTOR(7 downto 0);  -- 2 inputs 8-bit
+    ALU_Sel  : in  STD_LOGIC_VECTOR(2 downto 0);  -- 1 input 4-bit for selecting function
+    ALU_Out   : out  STD_LOGIC_VECTOR(7 downto 0); -- 1 output 8-bit 
+    Carryout : out std_logic        -- Carryout flag
+    );
 end ALU_8bit;
 
 architecture Behavioral of ALU_8bit is
 
+signal ALU_Result : std_logic_vector (7 downto 0);
+signal tmp: std_logic_vector (8 downto 0);
+
 begin
+   process(A,B,ALU_Sel)
+ begin
+  case(ALU_Sel) is
+  when "000" => -- Addition
+   ALU_Result <= A + B ; 
+  when "001" => -- Logical or
+   ALU_Result <= A or B;
+  when "010" => -- Logical and 
+   ALU_Result <= A and B;
+  when "011" => -- Zero
+   ALU_Result <= "00000000" ;
+  when "100" => -- one bit addition
+  ALU_Result <=  ;
+  when "101" => -- Logical shift left
+   ALU_Result <= std_logic_vector(unsigned(A) sll N);
 
-
+  when others => ALU_Result <= A + B ; 
+  end case;
+ end process;
+ ALU_Out <= ALU_Result; -- ALU out
+ tmp <= ('0' & A) + ('0' & B);
+ Carryout <= tmp(8); -- Carryout flag
 end Behavioral;
 
