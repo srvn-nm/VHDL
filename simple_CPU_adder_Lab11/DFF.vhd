29afcdef25b6bcc3821fb91c1befd39336a49866
Sarvin Nami <srvn0nm@gmail.com>
2022-06-03 13:14:18 +0430

    creating dff to use in register
    Signed-off-by: Sarvin Nami <srvn0nm@gmail.com>

---
+++ simple_CPU_adder_Lab11/DFF.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:13:49 06/03/2022 
+-- Design Name: 
+-- Module Name:    DFF - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DFF is
+    Port ( d : in  STD_LOGIC;
+           enable : in  STD_LOGIC;
+           q : out  STD_LOGIC);
+end DFF;
+
+architecture Behavioral of DFF is
+
+begin
+
+
+end Behavioral;
+
